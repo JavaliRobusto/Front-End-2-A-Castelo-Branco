d07c6092a9419386b15b4ad91a6bdfd6368cf539
JavaliRobusto <125268729+JavaliRobusto@users.noreply.github.com>
2023-03-31 12:05:27 -0300

    Create calculadora.js

---
+++ calculadora.js
@@ -0,0 +1,29 @@
+function somar (num_1,num_2){
+
+    console.log (num_1 + num_2);
+  
+
+};
+somar(2,4);
+      
+function subtrair (num_1,num_2){
+
+    console.log (num_1 - num_2);
+};
+
+subtrair(2,4);
+
+
+function multiplicar (num_1,num_2){
+
+    console.log (num_1 * num_2);
+};
+
+multiplicar(2,4);
+
+function dividir (num_1,num_2){
+
+    console.log (num_1 / num_2);
+};
+
+dividir(2,4)
